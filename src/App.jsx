78c2894297d77ce4ff5ae352ed234708cee23977
pushpendra singh <pushpendrasingh1919@gmail.com>
2024-09-09 18:03:09 +0530

    edit function add

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,59 @@
-import React,{useState} from 'react'
-import Todoinput from './Components/Todoinput'
+import React, { useState } from 'react';
+import Todoinput from './Components/Todoinput';
 import Todolist from './Components/Todolist';
 
-
 function App() {
-  const [listTodo,setListTodo]=useState([]);
-  let addList=(inputText)=>{
-   if(inputText!=='') setListTodo([...listTodo,inputText]);
-  }
-  const deleteListItem=((key)=>{
-    let newListTodo=[...listTodo];
-  newListTodo.splice(key,1)
-setListTodo([...newListTodo])})
-  
+  const [listTodo, setListTodo] = useState([]);
+  const [editIndex, setEditIndex] = useState(null); 
+  const [editText, setEditText] = useState(''); 
 
+  const addList = (inputText) => {
+    if (inputText !== '') {
+      setListTodo([...listTodo, inputText]);
+    }
+  };
 
+  const deleteListItem = (key) => {
+    const newListTodo = listTodo.filter((_, index) => index !== key);
+    setListTodo(newListTodo);
+    if (editIndex === key) {
+      setEditIndex(null); 
+    }
+  };
 
-const saveEdit=()=>{
-  setTodos(listTodo.map(listItem,i))
-}
+  const startEdit = (index) => {
+    setEditIndex(index);
+    setEditText(listTodo[index]);
+  };
+
+  const saveEdit = (index) => {
+    const updatedList = listTodo.map((item, i) => (i === index ? editText : item));
+    setListTodo(updatedList);
+    setEditIndex(null); 
+  };
+
   return (
-    <div  className=' justify-center items-center ml-[40%] m-[2%]'>
-<div> 
-<h1 className="text-3xl font-bold ml-[100px] m-5">
-      TO DO LIST
-    </h1>
-  <Todoinput addList={addList}/>
-<hr />
-{listTodo.map((listItem,i)=>{
-  return(  <Todolist  key={i} index={i} item={listItem} deleteItem={deleteListItem}/>)
-  
-})}
-</div>
-    </div>  ) 
+    <div className='justify-center items-center ml-[40%] m-[2%]'>
+      <div>
+        <h1 className="text-3xl font-bold ml-[100px] m-5">TO DO LIST</h1>
+        <Todoinput addList={addList} />
+        <hr />
+        {listTodo.map((listItem, i) => (
+          <Todolist
+            key={i}
+            index={i}
+            item={listItem}
+            deleteItem={deleteListItem}
+            editItem={startEdit}
+            isEditing={editIndex === i}
+            editText={editText}
+            setEditText={setEditText}
+            saveEdit={() => saveEdit(i)}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/Components/Todolist.jsx
+++ src/Components/Todolist.jsx
@@ -1,34 +1,47 @@
-import React from 'react'
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {  faPen } from '@fortawesome/free-solid-svg-icons';
+import { faTrashCan, faSave} from '@fortawesome/free-solid-svg-icons';
 
-
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {faTrashCan,faPen} from '@fortawesome/free-solid-svg-icons'
-
-function Todolist(props) {
+function Todolist({ index, item, deleteItem, editItem, isEditing, editText, setEditText, saveEdit }) {
   return (
-    <div >
-<div className="flex items-center max-w-xs ml-5 m-3 p-2 border border-gray-300 rounded-md">
-
-  <input type="text" className="flex-1 bg-transparent border-none p-2" placeholder={props.item} readOnly />
-
-
-  <FontAwesomeIcon
-    className="ml-2 cursor-pointer"
-    icon={faPen} onClick={()=>props.editText()}
-  />
-
-  <input type="checkbox" className="checkbox cursor-pointer checkbox-success ml-2" />
-
-  
-  <FontAwesomeIcon
-    className="ml-2 cursor-pointer"
-    onClick={() => props.deleteItem(props.index)}
-    icon={faTrashCan}
-  />
-</div>
-  </div>
-
-  )
+    <div className="flex items-center max-w-xs ml-5 m-3 p-2 border border-gray-300 rounded-md">
+      {isEditing ? (
+        <>
+          <input
+            type="text"
+            className="flex-1 bg-transparent border p-2"
+            value={editText}
+            onChange={(e) => setEditText(e.target.value)}
+          />
+             <FontAwesomeIcon className="ml-2 cursor-pointer" icon={faSave} 
+            onClick={saveEdit}
+          />
+        </>
+      ) : (
+        <>
+          <input
+            type="text"
+            className="flex-1 bg-transparent border-none p-2"
+            value={item}
+            readOnly
+          />
+        
+          <FontAwesomeIcon
+            className="ml-2 cursor-pointer"
+            icon={faPen}
+            onClick={() => editItem(index)}
+          />
+        </>
+      )}
+      <input type="checkbox" className="checkbox cursor-pointer checkbox-success ml-2" />
+      <FontAwesomeIcon
+        className="ml-2 cursor-pointer"
+        onClick={() => deleteItem(index)}
+        icon={faTrashCan}
+      />
+    </div>
+  );
 }
 
-export default Todolist
+export default Todolist;
